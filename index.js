f8e5db3010e8e6e443dc75c322b4097bbeaf542d
kostyatimoshuk <kostya.timoshuk@gmail.com>
2022-09-08 14:16:02 +0200

    fix5

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const port = 5000; 
 
 const corsOptions = { 
-  origin: "*",
+  origin: ["http://localhost:8080"],
   credentials: true,
   methods: ['PUT, GET, POST, OPTIONS, DELETE'],
   allowHeaders: ["DNT,X-CustomHeader,X-LANG,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,X-Api-Key,X-Device-Id,Access-Control-Allow-Origin,Authorization,Accept-Language,Api-Key"],
@@ -28,7 +28,7 @@ app.post('/cookie', (req, res) => {
   res.json({  status: 200, message : 'Hello, man!' });
 })
 
-app.post('/cookie-check', (req, res) => {
+app.get('/cookie-check', (req, res) => {
   console.log(req.cookie)
 
   res.json({  status: 200, message : 'checking!' });
